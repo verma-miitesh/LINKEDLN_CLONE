cdba431fa50f5ab12b6c35b1d4d313641c4f6a02
Miitesh Verma <vermamiitesh@gmail.com>
2025-07-06 22:02:24 +0530

    Before Responsiveness

--- frontend/src/Components/Navbar/index.jsx
+++ frontend/src/Components/Navbar/index.jsx
@@ -39,7 +39,9 @@ function NavBarComponent() {
                     {authState.profileFetched && <div>
                         <div style={{ display: "flex", gap: "1.2rem", color: "black" }}>
                             <p>Hey, {authState.user?.userId?.name || "Guest"}</p>
-                            <p style={{ fontWeight: "bold", cursor: "pointer" }}>Profile</p>
+                            <p onClick={()=>{
+                                router.push("/profile");
+                            }} style={{ fontWeight: "bold", cursor: "pointer" }}>Profile</p>
                             <p onClick={()=>{
                                 localStorage.removeItem("token")
                                 router.push("/login")
