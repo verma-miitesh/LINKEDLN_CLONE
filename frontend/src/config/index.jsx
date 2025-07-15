3af9cf522a06554f5c42bc481a497af48fe3b24c
Miitesh Verma <vermamiitesh@gmail.com>
2025-07-15 10:02:10 +0530

    Update ClientServer baseURL to render backend

--- frontend/src/config/index.jsx
+++ frontend/src/config/index.jsx
@@ -1,6 +1,6 @@
 const {default:axios}=require("axios");
  
-export const BASE_URL="http://localhost:8080"
+export const BASE_URL="https://linkedln-clone-lokc.onrender.com"
 
 
 export const clientServer=axios.create({
